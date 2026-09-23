3a0f23ebf287a3bbf6abb88dd288ffaa3efb1ffb
Jon Perry <jonperry113@gmail.com>
2026-09-23 12:37:36 -0400

    chore: update project configuration and build setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.jonnyramen.canvasdrawing',
-  appName: 'Canvas Drawing',
+  appId: 'io.jonnyramen.canvasdrawer',
+  appName: 'Canvas Drawer',
   webDir: 'www'
 };
 
